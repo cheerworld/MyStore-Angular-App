0380f58b824ec10b31f994966955afbce53ab0ae
Yuguo Zhao <cheerzhao77@gmail.com>
2021-09-18 23:14:49 -0500

    add router and get product detail in product-item-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: ':productName', component: ProductItemDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductItemComponent } from '../product-item/product-item.component';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
+  //products: Product[] = [];
 
-  constructor() { }
+  product: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    let name = this.route.snapshot.params.productName;
 
+    let cap1stLetter = name.charAt(0).toUpperCase() + name.slice(1);
+
+    console.log(cap1stLetter);
+
+    this.productsService.getProducts().subscribe((res) => {
+      const products = res;
+
+      this.product = products.filter((item) => {
+        console.log(item.name);
+        return item.name === cap1stLetter;
+      });
+
+      console.log(this.product);
+    });
+  }
 }
